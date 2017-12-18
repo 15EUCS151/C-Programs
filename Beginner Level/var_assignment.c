9f3ff435247e7f9ddb25627538677d554437fe88
15EUCS151 <34640761+15EUCS151@users.noreply.github.com>
2017-12-18 15:07:57 +0530

    Create var_assignment.c

---
+++ Beginner Level/var_assignment.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+char n[40]="Sachin Ramesh Tendulkar";
+char nn[25]="Little Master";
+int runs = 18426;
+float bat_avg = 44.83;
+int matches = 463;
+printf("\n %s",n);
+printf("\n %s",nn);
+printf("\n %d",runs);
+printf("\n %.2f",bat_avg);
+printf("\n %d",matches); 
+}
+
+
+
+
