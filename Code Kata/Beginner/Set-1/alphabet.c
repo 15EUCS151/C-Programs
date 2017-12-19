60a886c7984a5dc1b46354a2e2d1b5f8045487f8
15EUCS151 <34640761+15EUCS151@users.noreply.github.com>
2017-12-19 21:57:52 +0530

    Create alphabet.c

---
+++ Code Kata/Beginner/Set-1/alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+	char c;
+	printf("\n Enter a character to check whether it is an alphabet or not:");
+	scanf("%c",&c);
+	if((c>='a' && c<='z')||(c>='A' && c<='Z'))
+	  printf("ALPHABET");
+	else
+	  printf("NOT AN ALPHABET");
+	return 0;
+}
