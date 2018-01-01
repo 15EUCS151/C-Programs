4f780659b852e522edda5b8b802a08ff15b88fa9
15EUCS151 <34640761+15EUCS151@users.noreply.github.com>
2018-01-01 20:29:48 +0530

    Update file1.c

--- Pro/file1.c
+++ Pro/file1.c
@@ -1,3 +1,17 @@
+//test.txt
+
+#include <stdio.h>
+main(){
+  FILE *fp;
+  fp = fopen("/tmp/test.txt", "w+");
+  fprintf(fp, "This is testing for fprintf...\n");
+  fputs("This is testing for fputs...\n", fp);
+  fclose(fp);
+}
+
+
+
+
 #include <stdio.h>
 main(){ 
   FILE *fp; 
