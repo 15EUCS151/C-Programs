cd24161c1f28771e7860af8eedbd253e5a833710
15EUCS151 <34640761+15EUCS151@users.noreply.github.com>
2018-03-06 21:55:16 +0530

    Create max_element.c

---
+++ Code Kata/Beginner/SET-3/max_element.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+	int i,n,arr[100000],max=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	max = arr[0];
+	for(i=1;i<n;i++){
+		if(arr[i]>max)
+		max = arr[i];
+	}
+	printf("%d",max);
+	return 0;
+}
