fb461f6ed948285fdc510036741e862e1556caac
15EUCS151 <34640761+15EUCS151@users.noreply.github.com>
2018-03-09 11:41:34 +0530

    Update ap.c

--- Code Kata/Beginner/SET-3/ap.c
+++ Code Kata/Beginner/SET-3/ap.c
@@ -2,9 +2,9 @@
 int main()
 {
 	int n,d,f,i,sum=0;
-	printf("enter the series");
+	printf("\nEnter the series:");
 	scanf("%d",&n);
-	printf("enter the difference and first term");
+	printf("\nEnter the difference and first term:");
 	scanf("%d%d",&d,&f);
 	for(i=0;i<n;i++)
 	{
@@ -12,5 +12,5 @@ int main()
 		f=f+d;
 	}
 printf("sum is:%d",sum);
-return 0;;
+return 0;
 }
